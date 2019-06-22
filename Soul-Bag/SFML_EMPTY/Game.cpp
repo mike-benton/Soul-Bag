f996b13e68679e935fbe18127125a07e9f98eca5
Mike Benton <mxb3183@rit.edu>
2019-06-21 20:07:50 -0400

    initial commit?

---
+++ Soul-Bag/SFML_EMPTY/Game.cpp
@@ -0,0 +1,20 @@
+// SFML_EMPTY.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <SFML/Graphics.hpp> 
+void init();
+void update();
+
+int main() {
+    std::cout << "Hello World!\n"; 
+}
+
+void init() {
+
+}
+
+void update() {
+
+}
